062f66e2cf9a3a3ce416a542cc689ac68494526d
Robert Gabriel <robert_gabriel@outlook.com>
2015-10-25 12:23:58 +0000

    Added thesaures + word counts

--- test.js
+++ test.js
@@ -1,37 +1,59 @@
 var request = require("request");
-const chalk = require('chalk');
+var chalk = require('chalk');
+var sanitizeHtml = require('sanitize-html');
+var thesaurus = require("thesaurus");
 var stdin = process.openStdin();
+
 printWelcome();
+
 stdin.addListener("data", function(d) {
 	switch (d.toString().trim()) {
 		case "1":
-			day = "Sunday";
+			test();
 			break;
 		case "2":
 			makeCall();
 			break;
 		case "3":
-			day = "Tuesday";
+			thesaures();
 			break;
 		default:
 			printWelcome();
 	}
 });
 
+function test() {
+	if (['a', 'b', 'c'].indexOf(str) >= 0) {
+		//do something
+	}
+}
+
 function printWelcome() {
-	console.log(chalk.blue("Welcome to Robin text \n") + '1: Run Bench Test  \n2: Get html content' + chalk
-		.red('!'));
+	console.log(chalk.blue.bold("Welcome to Robin text \n") +
+		'1: Run Bench Test  \n2: Get html content \n3: thesaures' + chalk.red('!'));
+}
+
+function thesaures() {
+	var stdin = process.openStdin();
+	console.log(chalk.blue.bold("Please Enter a word : "));
+	stdin.addListener("data", function(d) {
+        
+		var thesaurusList = thesaurus.find(d.toString().trim());
+		console.log("Amount of Words : " + chalk.green(thesaurusList.length));
+	
+    });
 }
 
 function makeCall() {
 	request({
-		uri: "http://www.projectbird.com",
+		uri: "http://reddit.com",
 	}, function(error, response, body) {
-		//console.log(chalk.blue(body) + 'World' + chalk.red('!'));
-		var removeCss = body.replace(/<style>.*?<\/style>/g, '');
-		var removeLine = removeCss.replace(/::?[^ ,:.]+/g, '');
-		var removeJavascript = removeCss.replace(/<script>.*?<\/script>/g, '');
+		var removeJavascript = sanitizeHtml(body);
 		var test = removeJavascript.replace(/<(?:.|\n)*?>/gm, '');
-		console.log(test);
+		var removespacing = test.replace(/\s\s+/g, ' ');
+		var words = removespacing.split(" ");
+		console.log(chalk.green("Words"));
+		console.log(words);
+		console.log("Amount of Words : " + chalk.green(words.length));
 	});
 }
