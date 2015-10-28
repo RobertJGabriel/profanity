3c7d53bfaa11146607df6d732537a27ec77c857d
Robert Gabriel <robert_gabriel@outlook.com>
2015-10-28 02:00:44 +0000

    Added compare test

--- test.js
+++ test.js
@@ -2,7 +2,15 @@ var request = require("request");
 var chalk = require('chalk');
 var sanitizeHtml = require('sanitize-html');
 var thesaurus = require("thesaurus");
+
+
+
 var stdin = process.openStdin();
+var wasThereList = [];
+var thesaurusList = [];
+var words = [];
+var thesaurusCount = 0;
+var wordsCount = 0;
 
 printWelcome();
 
@@ -15,45 +23,66 @@ stdin.addListener("data", function(d) {
 			makeCall();
 			break;
 		case "3":
-			thesaures();
+			thesaures2();
 			break;
+		case "4":
+			print();
+			break;
+		case "5":
+			process.exit();
 		default:
 			printWelcome();
 	}
 });
 
 function test() {
-	if (['a', 'b', 'c'].indexOf(str) >= 0) {
-		//do something
+	makeCall();
+	thesaures2();
+	compareStatus();
+}
+
+function compareStatus() {
+	for (var i = 0; i < thesaurusList.length; i++) {
+		if (words.indexOf(thesaurusList[i]) > -1) {
+			wasThereList.push(thesaurusList[i]);
+		}
 	}
+	console.log('Is there');
+	console.log(wasThereList);
+	printWelcome();
 }
 
 function printWelcome() {
 	console.log(chalk.blue.bold("Welcome to Robin text \n") +
-		'1: Run Bench Test  \n2: Get html content \n3: thesaures' + chalk.red('!'));
+		'1: Run Bench Test  \n2: Get html content \n3: thesaures \n4: print status \n5: close' + chalk.red(
+			'!'));
 }
 
-function thesaures() {
+function thesaures2() {
 	var stdin = process.openStdin();
 	console.log(chalk.blue.bold("Please Enter a word : "));
 	stdin.addListener("data", function(d) {
-        
-		var thesaurusList = thesaurus.find(d.toString().trim());
-		console.log("Amount of Words : " + chalk.green(thesaurusList.length));
-	
-    });
+		thesaurusList = thesaurus.find(d.toString().trim());
+		thesaurusCount = thesaurusList.length;
+	});
+	printWelcome();
+}
+
+function print() {
+	console.log(" Words on Website:\n" + words + "\nThesaurus Words:" + thesaurusList +
+		"\n Amount of Words:" + wordsCount + "\nThesaurus count:" + thesaurusCount);
+	printWelcome();
 }
 
 function makeCall() {
 	request({
-		uri: "http://reddit.com",
+		uri: "http://www.reddit.com",
 	}, function(error, response, body) {
 		var removeJavascript = sanitizeHtml(body);
 		var test = removeJavascript.replace(/<(?:.|\n)*?>/gm, '');
 		var removespacing = test.replace(/\s\s+/g, ' ');
-		var words = removespacing.split(" ");
-		console.log(chalk.green("Words"));
-		console.log(words);
-		console.log("Amount of Words : " + chalk.green(words.length));
+		words = removespacing.split(" ");
+		wordsCount = words.length;
 	});
+	printWelcome();
 }
