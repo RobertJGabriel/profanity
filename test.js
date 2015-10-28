4a6ecd694d9eff0dede1ce9f1957829caf58412e
Robert Gabriel <robert_gabriel@outlook.com>
2015-10-28 22:06:29 +0000

    Added : Final Version and Test
    I

--- test.js
+++ test.js
@@ -4,7 +4,6 @@ var sanitizeHtml = require('sanitize-html');
 var thesaurus = require("thesaurus");
 var util = require('util');
 var async = require('async');
-
 var stdin = process.openStdin();
 
 var wasThereList = [];
@@ -16,75 +15,63 @@ printWelcome();
 
 stdin.addListener("data", function(d) {
 	switch (d.toString().trim()) {
-        case "1":
+		case "1":
 			tests();
 			break;
 		case "2":
 			makeCall("http://sexetc.org/");
+            print();
 			break;
 		case "3":
-			thesaures2("sex");
+			setThesaures("sex");
+            print();
 			break;
 		case "4":
 			compareStatus();
 			break;
 		case "5":
 			process.exit();
-		case "6":
-			badWord("sex");
 		default:
 			printWelcome();
 	}
 });
 
-
-function tests(){
-
-// an example using an object instead of an array
-async.parallel({
-    one: function(callback){
-        setTimeout(function(){
-            callback(null, "ss");
-      
-        }, 200);
-    },
-    two: function(callback){
-        setTimeout(function(){
-            callback(null, "ss");
-             	makeCall("http://sexetc.org/");
-        }, 3600);
-    }
-    ,
-   three: function(callback){
-        setTimeout(function(){
-            callback(null, "ss");
-          thesaures2("sex");
-        }, 4500);
-    },
-    
-       four: function(callback){
-        setTimeout(function(){
-            callback(null, "ss");
-          compareStatus();
-        }, 6600);
-    }
-    
-},
-function(err, results) {
- setTimeout(function(){
-        print();
-        }, 7600);
-});
+function tests() {
+	async.parallel({
+		one: function(callback) {
+			setTimeout(function() {
+				callback(null, "ss");
+			}, 1000);
+		},
+		two: function(callback) {
+			setTimeout(function() {
+				callback(null, "ss");
+				makeCall("http://xhamster.com/");
+			}, 2000);
+		},
+		three: function(callback) {
+			setTimeout(function() {
+				callback(null, "ss");
+				setThesaures("sex");
+			}, 3000);
+		},
+		four: function(callback) {
+			setTimeout(function() {
+				callback(null, "ss");
+				compareStatus();
+			}, 4000);
+		}
+	}, function(err, results) {
+		setTimeout(function() {
+			print();
+		}, 5000);
+	});
 }
 
 function compareStatus() {
 	for (var i = 0; i < words.length; i++) {
-		console.log(chalk.green.bold(words[i]) + "\n" );
-            badWord(words[i]);
-    }
-	print();
-
-
+		badWord(words[i]);
+	}
 }
 
 function printWelcome() {
@@ -93,44 +80,46 @@ function printWelcome() {
 			'!'));
 }
 
-function thesaures2(word) {
-	console.log("thesaures \n");
+function setThesaures(word) {
 	thesaurusList = thesaurus.find(word);
 }
 
 function print() {
-	console.log(" Words on Website:\n" + words + "\nThesaurus Words:" + thesaurusList +
-		"\n Amount of Words:" + words.length + "\nThesaurus count:" + thesaurusList.length + "\nBad word : \n " +
-		wasThereList);
+    console.log(chalk.underline.red("Words on Website") + "\n");
+    console.log(words + "\n \n");
+    
+    console.log(chalk.underline.green("Thesaurus Words") + "\n");
+    console.log(thesaurusList + "\n \n");
+    
+    console.log(chalk.underline.green("Banned Words") + "\n");
+    console.log(wasThereList + "\n \n");
+    
+    console.log(chalk.underline.green("Amount of Words : ") + chalk.red(words.length) + "\n");
+    console.log(chalk.underline.green("Thesaurus Count : ") + chalk.red(thesaurusList.length) + "\n");
+    console.log(chalk.underline.green("Banned Count : ") + chalk.red(wasThereList.length) + "\n");
 }
 
 function badWord(word) {
 	request({
 		uri: "http://www.wdyl.com/profanity?q=" + encodeURI(word),
-          async:   false,
+		async: false,
 	}, function(error, response, body) {
 		obj = JSON.parse(body);
-
 		if (obj.response === "true") {
-
-            wasThereList.push(word);
-	       console.log(chalk.red(word));
-		
+			wasThereList.push(word.toLowerCase());
 		}
 	});
+
 }
 
 function makeCall(urls) {
-	console.log("Made call \n");
 	request({
 		uri: urls,
 	}, function(error, response, body) {
 		var removeJavascript = sanitizeHtml(body);
 		var test = removeJavascript.replace(/<(?:.|\n)*?>/gm, '');
 		var removespacing = test.replace(/\s\s+/g, ' ');
-		    words = removespacing.split(" ");
-		  
-		console.log(words);
-
+		words = removespacing.split(" ");
 	});
 }
+
