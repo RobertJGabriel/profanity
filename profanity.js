0add084cdd2cd39e4b79e021aa5dd08e0c541505
Robert Gabriel <robert_gabriel@outlook.com>
2015-10-29 14:06:21 +0000

    Adde Module Test

--- profanity.js
+++ profanity.js
@@ -4,38 +4,15 @@ var sanitizeHtml = require('sanitize-html');
 var thesaurus = require("thesaurus");
 var util = require('util');
 var async = require('async');
-var stdin = process.openStdin();
 
 var wasThereList = [];
 var thesaurusList = [];
 var words = [];
 var percent = 0;
 
-printWelcome();
-
-
-stdin.addListener("data", function(d) {
-	switch (d.toString().trim()) {
-		case "1":
-			tests();
-			break;
-		case "2":
-			makeCall("http://sexetc.org/");
-            print();
-			break;
-		case "3":
-			setThesaures("sex");
-            print();
-			break;
-		case "4":
-			compareStatus();
-			break;
-		case "5":
-			process.exit();
-		default:
-			printWelcome();
-	}
-});
+module.exports = function(website,terms) {
+    return tests();
+}
 
 function tests() {
 	async.parallel({
@@ -75,11 +52,7 @@ function compareStatus() {
 	}
 }
 
-function printWelcome() {
-	console.log(chalk.blue.bold("Welcome to Robin text \n") +
-		'1: Run Bench Test  \n2: Get html content \n3: thesaures \n4: Compare \n5: close' + chalk.red(
-			'!'));
-}
+
 
 function setThesaures(word) {
 	thesaurusList = thesaurus.find(word);
